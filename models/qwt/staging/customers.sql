b1f89bdb01ed077d38790c7f6111119f9a94fa4d
Avunuri Rakesh <rakesh.avunuri971@gmail.com>
2024-02-22 04:56:56 +0000

    dev phase 2 completed

--- models/qwt/staging/customers.sql
+++ models/qwt/staging/customers.sql
@@ -1,3 +1,3 @@
-{{config(materialized='table',schema = 'STAGING',
+{{config(materialized='table',
 enabled=true,  alias = 'stg_customers')}}
 select * from {{env_var('dbt_sourcedb','qwt')}}.{{env_var('dbt_sourceschema','raw')}}.customers
--- models/qwt/staging/stg_suppliers_xml.sql
+++ models/qwt/staging/stg_suppliers_xml.sql
@@ -1,2 +1,2 @@
-{{config(materialized='table',schema= 'staging') }}
+{{config(materialized='table') }}
 select * from {{env_var('dbt_sourcedb','qwt')}}.{{env_var('dbt_sourceschema','raw')}}.SUPPLIERS_XML
